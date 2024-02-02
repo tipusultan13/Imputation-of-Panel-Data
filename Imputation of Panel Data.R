af461dfccc2147e10d3595cc7c65661dbd89a9d7
tipusultan13 <156308034+tipusultan13@users.noreply.github.com>
2024-02-02 19:36:12 +0100

    upload data

--- Imputation of Panel Data.R
+++ Imputation of Panel Data.R
@@ -1,2 +1,11 @@
+###################################
+## Tipu Sultan
+###################################
+
+# Set Directory
 setwd("/Users/tipusultan/Documents/GitHub/Imputation-of-Panel-Data")
+
+# Load Data
+LifeExpectancy <- read_excel('life expectancy.xlsx')
+View(LifeExpectancy)
 
