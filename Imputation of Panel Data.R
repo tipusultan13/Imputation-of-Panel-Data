dfd1277b81c63a36c3730f4a84845bb2c529c013
tipusultan13 <156308034+tipusultan13@users.noreply.github.com>
2024-04-30 08:20:59 +0200

    Update Imputation of Panel Data.R

--- Imputation of Panel Data.R
+++ Imputation of Panel Data.R
@@ -3,9 +3,24 @@
 ###################################
 
 # Set Directory
-setwd("/Users/tipusultan/Documents/GitHub/Imputation-of-Panel-Data")
+# setwd("/Users/tipusultan/Documents/GitHub/Imputation-of-Panel-Data")
+
+########################
+## Data Preparation
+########################
 
 # Load Data
-LifeExpectancy <- read_excel('life expectancy.xlsx')
+LifeExpectancy <- read_excel('life expectancy.xlsx', col_names = FALSE)
+LifeExpectancy <- as.data.frame(LifeExpectancy)
+LifeExpectancy <- LifeExpectancy[-1, ]
+colnames(LifeExpectancy) <- LifeExpectancy[1, ]
+LifeExpectancy <- LifeExpectancy[-1, ]
+LifeExpectancy <- LifeExpectancy[order(LifeExpectancy$`Country Name`), ]
+
+LifeExpectancy <- LifeExpectancy[, c('Country Name', 'Year', 'Health Expenditure %')]
 View(LifeExpectancy)
+
+
+
+
 
