70d2d139030d3aed3df95f1bda2918b5e5027035
tipusultan13 <156308034+tipusultan13@users.noreply.github.com>
2024-05-07 09:11:55 +0200

    Update Imputation of Panel Data.R

--- Imputation of Panel Data.R
+++ Imputation of Panel Data.R
@@ -228,10 +228,175 @@ summary(balanced_panel_data_mnar_10)
 
 
 
+########################
+## Missingness in Unalanced Panel
+########################
+
+#### MCAR ####
+##############
+
+p_mis_50 <- 0.50
+num_rows <- nrow(unbalanced_panel_data)  # Get the number of rows in balanced_panel_data
+mis_simulated_mcar_50 <- sample(1:num_rows, p_mis_50 * num_rows, replace = FALSE)    
+unbalanced_panel_data_mcar_50 <- unbalanced_panel_data
+unbalanced_panel_data_mcar_50[mis_simulated_mcar_50, 3] <- NA
+summary(unbalanced_panel_data_mcar_50)
+
+p_mis_30 <- 0.30
+num_rows <- nrow(unbalanced_panel_data)  # Get the number of rows in balanced_panel_data
+mis_simulated_mcar_30 <- sample(1:num_rows, p_mis_30 * num_rows, replace = FALSE)    
+unbalanced_panel_data_mcar_30 <- unbalanced_panel_data
+unbalanced_panel_data_mcar_30[mis_simulated_mcar_30, 3] <- NA
+summary(unbalanced_panel_data_mcar_30)
+
+p_mis_10 <- 0.10
+num_rows <- nrow(unbalanced_panel_data)  # Get the number of rows in balanced_panel_data
+mis_simulated_mcar_10 <- sample(1:num_rows, p_mis_10 * num_rows, replace = FALSE)    
+unbalanced_panel_data_mcar_10 <- unbalanced_panel_data
+unbalanced_panel_data_mcar_10[mis_simulated_mcar_10, 3] <- NA
+summary(unbalanced_panel_data_mcar_10)
+
+#### MAR ####
+#############
+
+#### 50% ####
+unbalanced_panel_data_mar_50 <- unbalanced_panel_data
+set.seed(123)
+p_mis_50 <- 0.5  # 50% missingness
+
+# Model missingness via linear regression model
+# Depending on x1, x2, and random error
+mis_simulated_mar_50 <- 0.5 + 2 * unbalanced_panel_data_mar_50$x1 - 
+  0.7 * unbalanced_panel_data_mar_50$x2 + 
+  rnorm(nrow(unbalanced_panel_data_mar_50), 0, 3)
+
+# All below the 50% quantile are set to missing
+mis_simulated_mar_50 <- mis_simulated_mar_50 < quantile(mis_simulated_mar_50, p_mis_50)
+
+# Proportion of missing data
+mean(as.numeric(mis_simulated_mar_50))
+
+# Set values to NA in y where missingness occurs
+unbalanced_panel_data_mar_50$y[mis_simulated_mar_50] <- NA
+
+# Summary of y after introducing missingness
+summary(unbalanced_panel_data_mar_50)
 
 
+#### 30% ####
+unbalanced_panel_data_mar_30 <- unbalanced_panel_data
+set.seed(123)
+p_mis_50 <- 0.3  # 30% missingness
+
+# Model missingness via linear regression model
+# Depending on x1, x2, and random error
+mis_simulated_mar_30 <- 0.5 + 2 * unbalanced_panel_data_mar_30$x1 - 
+  0.7 * unbalanced_panel_data_mar_30$x2 + 
+  rnorm(nrow(unbalanced_panel_data_mar_30), 0, 3)
+
+# All below the 50% quantile are set to missing
+mis_simulated_mar_30 <- mis_simulated_mar_30 < quantile(mis_simulated_mar_30, p_mis_30)
+
+# Proportion of missing data
+mean(as.numeric(mis_simulated_mar_30))
+
+# Set values to NA in y where missingness occurs
+unbalanced_panel_data_mar_30$y[mis_simulated_mar_30] <- NA
+
+# Summary of y after introducing missingness
+summary(unbalanced_panel_data_mar_30)
 
 
+#### 10% ####
+
+unbalanced_panel_data_mar_10 <- unbalanced_panel_data
+set.seed(123)
+p_mis_50 <- 0.1  # 10% missingness
+
+# Model missingness via linear regression model
+# Depending on x1, x2, and random error
+mis_simulated_mar_10 <- 0.5 + 2 * unbalanced_panel_data_mar_10$x1 - 
+  0.7 * unbalanced_panel_data_mar_10$x2 + 
+  rnorm(nrow(unbalanced_panel_data_mar_10), 0, 3)
+
+# All below the 50% quantile are set to missing
+mis_simulated_mar_10 <- mis_simulated_mar_10 < quantile(mis_simulated_mar_10, p_mis_10)
+
+# Proportion of missing data
+mean(as.numeric(mis_simulated_mar_10))
+
+# Set values to NA in y where missingness occurs
+unbalanced_panel_data_mar_10$y[mis_simulated_mar_10] <- NA
+
+# Summary of y after introducing missingness
+summary(unbalanced_panel_data_mar_10)
+
+#### MNAR ####
+##############
+
+#### 50% ####
+#### Probabilistic, Linear Regression model, Real data ####
+
+p_mis_50 <- .50
+unbalanced_panel_data_mnar_50 <- unbalanced_panel_data
+# model missingness by logistic regression (probit):
+# the missing of a value now also depends on counts itself
+mis_simulated_mnar_50 <-0.5 + 1 * unbalanced_panel_data_mnar_50$x1 - 
+  0.7 * unbalanced_panel_data_mnar_50$x2 - 
+  5 * unbalanced_panel_data_mnar_50$y + rnorm(nrow(unbalanced_panel_data), 0, 3)
+mis_simulated_mnar_50 <- mis_simulated_mnar_50 < quantile(mis_simulated_mnar_50, p_mis_50)
+unbalanced_panel_data_mnar_50$y[mis_simulated_mnar_50] <- NA
+summary(unbalanced_panel_data_mnar_50)
+
+#### 30% ####
+
+p_mis_30 <- .30
+unbalanced_panel_data_mnar_30 <- unbalanced_panel_data
+# model missingness by logistic regression (probit):
+# the missing of a value now also depends on counts itself
+mis_simulated_mnar_30 <-0.5 + 1 * unbalanced_panel_data_mnar_30$x1 - 
+  0.7 * unbalanced_panel_data_mnar_30$x2 - 
+  5 * unbalanced_panel_data_mnar_30$y + rnorm(nrow(unbalanced_panel_data), 0, 3)
+mis_simulated_mnar_30 <- mis_simulated_mnar_30 < quantile(mis_simulated_mnar_30, p_mis_30)
+unbalanced_panel_data_mnar_30$y[mis_simulated_mnar_30] <- NA
+summary(unbalanced_panel_data_mnar_30)
+
+#### 10% ####
+
+p_mis_10 <- .10
+unbalanced_panel_data_mnar_10 <- unbalanced_panel_data
+# model missingness by logistic regression (probit):
+# the missing of a value now also depends on counts itself
+mis_simulated_mnar_10 <-0.5 + 1 * unbalanced_panel_data_mnar_10$x1 - 
+  0.7 * unbalanced_panel_data_mnar_10$x2 - 
+  5 * unbalanced_panel_data_mnar_10$y + rnorm(nrow(unbalanced_panel_data), 0, 3)
+mis_simulated_mnar_10 <- mis_simulated_mnar_10 < quantile(mis_simulated_mnar_10, p_mis_10)
+unbalanced_panel_data_mnar_10$y[mis_simulated_mnar_10] <- NA
+summary(unbalanced_panel_data_mnar_10)
+
+
+###################
+### Data Sets
+###################
+
+# balanced_panel_data_mcar_50
+# balanced_panel_data_mcar_30
+# balanced_panel_data_mcar_10
+# balanced_panel_data_mar_50
+# balanced_panel_data_mar_30
+# balanced_panel_data_mar_10
+# balanced_panel_data_mnar_50
+# balanced_panel_data_mnar_30
+# balanced_panel_data_mnar_10
+# unbalanced_panel_data_mcar_50
+# unbalanced_panel_data_mcar_30
+# unbalanced_panel_data_mcar_10
+# unbalanced_panel_data_mar_50
+# unbalanced_panel_data_mar_30
+# unbalanced_panel_data_mar_10
+# unbalanced_panel_data_mnar_50
+# unbalanced_panel_data_mnar_30
+# unbalanced_panel_data_mnar_10
 
 
 
